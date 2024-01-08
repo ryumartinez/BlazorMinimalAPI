152f0968893b771e6c094949998417d8b38cef8d
ryumartinez <ryueduardomartinez@outlook.es>
2024-01-08 07:45:26 -0300

    deleted mvc and slices projects
    i will be using only the minimal apis

--- Samples/BlazorMinimalApi.Slices/Lib/IRouteDefinition.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Builder;
-
-namespace BlazorMinimalApis.Slices.Lib;
-
-public interface IRouteDefinition
-{
-	void Map(WebApplication app);
-}
--- Samples/BlazorMinimalApis.Mvc/Lib/IRouteDefinition.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Builder;
-
-namespace BlazorMinimalApis.Mvc.Lib;
-
-public interface IRouteDefinition
-{
-	void Map(WebApplication app);
-}
